fbf9bd7169b9afd4840b2a839e5f03f9d86e0e9e
Milene Taborda <milenetaborda1@outlook.com>
2022-06-29 14:58:44 -0300

    chore: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-react-ts-ghactions-template/',
+  base: '/',
   plugins: [react(), tsconfigPaths()],
   build: {
     sourcemap: true
