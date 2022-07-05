b2f9e83a39c98240a3abb8daf256f7d17d45db93
Milene Taborda <milenetaborda1@outlook.com>
2022-07-05 17:11:44 -0300

    feat: create button upload image

--- src/components/Steps/FeedbackContentStep.tsx
+++ src/components/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from 'phosphor-react';
+import { ArrowLeft, Camera } from 'phosphor-react';
 import { CloseButton } from 'src/components/CloseButton';
 import { IFeedbackType } from 'src/components/WidgetForm';
 import { feedbackTypes } from 'src/constants/feedbackTypes';
@@ -37,6 +37,13 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
         />
 
         <footer className="flex gap-2 mt-2">
+          <button
+            type="button"
+            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offsite-2 focus:ring-offsite-zinc-900 focus:ring-brand-500 transition-colors "
+          >
+            <Camera className="w-6 h-6" />
+          </button>
+
           <button
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offsite-2 focus:ring-offsite-zinc-900 focus:ring-brand-500 transition-colors"
